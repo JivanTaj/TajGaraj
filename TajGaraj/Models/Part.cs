2406a8167fe7ac5efca0abd62b817fe812e9cb58
Jivan Taj <TAJJA1@ETSU.EDU>
2024-05-14 18:21:15 -0400

    Add project files.

---
+++ TajGaraj/Models/Part.cs
@@ -0,0 +1,11 @@
+﻿namespace TajGaraj.Models
+{
+    public class Part
+    {
+        public int PartID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+
+    }
+}
