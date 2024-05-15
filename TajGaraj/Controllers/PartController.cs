d5ef0de9d119fd6995fe920d8229566d5477ab42
Jivan Taj <TAJJA1@ETSU.EDU>
2024-05-15 13:30:15 -0400

    part controller Index is working, still error on part create

---
+++ TajGaraj/Controllers/PartController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TajGaraj.Models;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace TajGaraj.Controllers
+{
+    public class PartsController : Controller
+    {
+        private string _path = "Data/partData/parts.json";
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        } 
+       
+        [HttpPost]
+        public async Task<ActionResult> Create(Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                // Load existing parts from the file
+                var parts = new List<Part>();
+                if (System.IO.File.Exists(_path))
+                {
+                    var json = await System.IO.File.ReadAllTextAsync(_path);
+                    parts = JsonSerializer.Deserialize<List<Part>>(json);
+                }
+
+                // Add the new part
+                parts.Add(part);
+
+                // Save all parts back to the file
+                var newJson = JsonSerializer.Serialize(parts);
+                await System.IO.File.WriteAllTextAsync(_path, newJson);
+                return RedirectToAction("Index");
+            }
+
+            return View(part);
+        }
+    }
+}
