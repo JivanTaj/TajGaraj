f2691e6b4651b492efcf3bd6fde333324eaf52fd
Jivan Taj <TAJJA1@ETSU.EDU>
2024-07-17 17:33:24 -0400

    catchup commit

---
+++ TajGaraj/Models/Options/PartCondition.cs
@@ -0,0 +1,11 @@
+﻿namespace TajGaraj.Models.Options
+{
+    public enum PartCondition
+    {
+        New,
+        Used,
+        Refurbished, 
+        Broken
+        // Add more conditions here as needed
+    }
+}
